1a4181b1be1b042a4135c80dbd7a12aa4406a2d4
ishaanbusi <ishaanbusi@gmail.com>
2025-08-07 16:08:32 +0530

    Updated Version of Source Code

--- knexfile.js
+++ knexfile.js
@@ -11,6 +11,16 @@ module.exports = {
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME
     },
+    pool: {
+      min: 0,              // Critical for container environments
+      max: 5,              // Lower max for Coolify/container constraints
+      acquireTimeoutMillis: 60000,
+      createTimeoutMillis: 30000,
+      destroyTimeoutMillis: 5000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100
+    },
     migrations: {
       directory: './migrations'
     }
