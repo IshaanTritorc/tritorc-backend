b79f2a1d64327408301d223b9c18dac8d02b114f
ishaanbusi <ishaanbusi@gmail.com>
2025-08-07 15:58:52 +0530

    Updated Version of Source Code

--- knexfile.js
+++ knexfile.js
@@ -1,24 +1,18 @@
+// knexfile.js
 require('dotenv').config();
 
-const sharedConfig = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT || 5432,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    directory: './migrations'
-  }
-};
-
 module.exports = {
-  development: sharedConfig,
-  production: sharedConfig
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    },
+    migrations: {
+      directory: './migrations'
+    }
+  }
 };
